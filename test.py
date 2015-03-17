b1989a1c8fe82aea28d99516648bca01ce0394bf
nisxiya <nisxiya@yeah.net>
2015-03-17 09:40:52 +0800

    some old files

---
+++ test.py
@@ -0,0 +1,4 @@
+mylist=['amc',232,'354','j8u']
+mylist.pop()
+print '___'
+print mylist
---
+++ using_sys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#Filename:using_sys.py
+import sys
+print 'The command line arguement are:'
+for i in sys.argv:
+    print i
+print '\n\nThe PYTHON is',sys.path,'\n'
